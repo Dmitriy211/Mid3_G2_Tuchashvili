067c922f0339786d6734939dc182c06ce74b8278
Dmitriy211 <dmitriy.211@mail.ru>
2017-03-06 16:58:19 +0600

    Train

---
+++ Mid3_G2_Tuchashvili/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;                                 // ██-██-██-██-██▄
+using System.Threading.Tasks;
+
+namespace Mid3_G2_Tuchashvili
+{
+    class Program
+    {
+        public static void Move()
+        {
+            while (true)
+            {
+                Console.Clear();
+                if (y == 3)
+                {
+                    Console.SetCursorPosition(x, y);
+                    Console.WriteLine(train);
+                    x++;
+                    Thread.Sleep(100);
+                }
+                if (x == 30)
+                    y++;
+                if (y == 4)
+                {
+                    Console.SetCursorPosition(x, y);
+                    Console.WriteLine(train2);
+                    x--;
+                    Thread.Sleep(100);
+                }
+                if (x == 0)
+                    y--;
+
+            }
+        }
+
+
+        public static string train, train2;
+        public static int x, y;
+
+        static void Main(string[] args)
+        {
+
+            train = "██-██-██-██-██▄";
+            train2 = "▄██-██-██-██-██";
+            x = 0;
+            y = 3;
+
+            Thread t = new Thread(new ThreadStart(Move));
+            t.Start();
+
+        }
+    }
+}
